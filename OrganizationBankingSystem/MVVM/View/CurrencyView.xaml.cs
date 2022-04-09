5e8f43e33e910ef9103c111366ced90bb17f773e
ilyat <ilyatsyb@gmail.com>
2022-04-09 21:44:26 +0300

    v100422R000008/feature/improve UI, sidebar icons open sections

--- OrganizationBankingSystem/MVVM/View/CurrencyView.xaml.cs
+++ OrganizationBankingSystem/MVVM/View/CurrencyView.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Text.Json;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
