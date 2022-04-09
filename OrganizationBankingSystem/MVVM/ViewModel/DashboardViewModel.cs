c5ccd195b045c568d9c9f7204617d6bcc0c7929c
ilyat <ilyatsyb@gmail.com>
2022-04-09 21:44:26 +0300

    100422R000008/feature/impove UI, sidebar icons open sections

--- OrganizationBankingSystem/MVVM/ViewModel/DashboardViewModel.cs
+++ OrganizationBankingSystem/MVVM/ViewModel/DashboardViewModel.cs
@@ -10,23 +10,8 @@ namespace OrganizationBankingSystem.MVVM.ViewModel
 {
     class DashboardViewModel
     {
-        //public dynamic exchangeRates;
-
-        //public dynamic ExchangeRates
-        //{
-        //    get;
-        //    set;
-        //}
         public DashboardViewModel()
         {
-            //string QUERY_URL = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=EUR&apikey=64SWH72PQKF16IA1";
-            //Uri queryUri = new Uri(QUERY_URL);
-
-            //using (WebClient client = new WebClient())
-            //{
-            //    dynamic json_data = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(client.DownloadString(queryUri));
-            //    exchangeRates = json_data;
-            //}
         }
     }
 }
