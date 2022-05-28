46adbb3bfc9812b027f0c0a0889fd4359c158562
ilyat <ilyatsybulenok@outlook.com>
2022-05-28 23:43:23 +0300

    v280522T000026/feature/add maximize window, improve UI

---
+++ OrganizationBankingSystem/Core/Converters/DecreaseConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace OrganizationBankingSystem.Core.Converters
+{
+    public class DecreaseConverter : MarkupExtension, IValueConverter
+    {
+        private static DecreaseConverter _instance;
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToInt32(value) - System.Convert.ToInt32(parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _instance ??= new DecreaseConverter();
+        }
+    }
+}
--- OrganizationBankingSystem/MainWindow.xaml.cs
+++ OrganizationBankingSystem/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using OrganizationBankingSystem.Core;
 using OrganizationBankingSystem.Core.Helpers;
+using System;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media.Animation;
 
 namespace OrganizationBankingSystem
 {
@@ -26,6 +28,37 @@ namespace OrganizationBankingSystem
             {
                 NotifierHelper.notifier.ShowWarningPropertyMessage("Отсутствует или является нестабильным подключение к сети Интернет. Это может повлиять на работу некоторых функций приложения");
             }
+        }
+
+        private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Maximized;
+
+            WindowBorder.Width = SystemParameters.PrimaryScreenWidth;
+            WindowBorder.Height = SystemParameters.PrimaryScreenHeight;
+            WindowBorder.CornerRadius = new CornerRadius(0);
+
+            borderContent.Width = SystemParameters.PrimaryScreenWidth - 160;
+            borderContent.CornerRadius = new CornerRadius(0);
+
+            int increaseWidthValue = 44;
+
+            if (ToggleButtonHidePanel.IsChecked == false)
+            {
+                increaseWidthValue = 160;
+            }
+
+            DoubleAnimation borderContentAnimation = new(borderContent.Width,
+                SystemParameters.PrimaryScreenWidth - increaseWidthValue,
+                new Duration(TimeSpan.FromSeconds(1)));
+
+            Storyboard.SetTargetName(borderContentAnimation, borderContent.Name);
+            Storyboard.SetTargetProperty(borderContentAnimation, new PropertyPath(WidthProperty));
+
+            Storyboard myStoryboard = new();
+            myStoryboard.Children.Add(borderContentAnimation);
+
+            myStoryboard.Begin(ToggleButtonHidePanel);
         }
     }
 }
